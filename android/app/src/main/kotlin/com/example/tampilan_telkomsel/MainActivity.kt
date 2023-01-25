3d24a59ebf73954a7f73800862c461967dacfa6a
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2023-01-26 00:51:06 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/tampilan_telkomsel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tampilan_telkomsel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'Wisgets/splash.dart';
+import 'routes/app_pages.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Future.delayed(Duration(seconds: 3)),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return SplashScreen();
+          } else {
+            return GetMaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: "Application",
+              initialRoute: Routes.LOGIN,
+              getPages: AppPages.routes,
+            );
+          }
+        });
+  }
+}
